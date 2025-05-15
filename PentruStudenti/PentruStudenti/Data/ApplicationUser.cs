977cc0bd5ec040f82ed9d0cfb05dc7ed6e75aba2
CosminCAS123 <cosmingamer2004@gmail.com>
2025-05-15 23:40:00 +0300

    lil stuff in registration, fixed ui and added some new fields for applicationuser

--- PentruStudenti/PentruStudenti/Data/ApplicationUser.cs
+++ PentruStudenti/PentruStudenti/Data/ApplicationUser.cs
@@ -5,7 +5,10 @@ namespace PentruStudenti.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-        public string DisplayName { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
 
         public string University { get; set; }
 
